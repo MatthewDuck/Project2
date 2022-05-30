7b4a2af58974d0e2396c21fcb92f9b0081968a16
Matthew Duck <MatthewDDuck@googlemail.com>
2022-05-30 11:41:53 +0100

    update and read methods

--- src/main/java/com/qa/project2/controllers/CharacterSheetController.java
+++ src/main/java/com/qa/project2/controllers/CharacterSheetController.java
@@ -1,8 +1,15 @@
 package com.qa.project2.controllers;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +20,34 @@ import com.qa.project2.services.CharacterSheetService;
 @RestController
 @RequestMapping("/character_sheet")
 public class CharacterSheetController {
-	
+
 	private CharacterSheetService service;
-	
+
 	public CharacterSheetController(CharacterSheetService service) {
 		super();
-		this.service=service;
+		this.service = service;
 	}
-	
+
 	@PostMapping("/create")
 	public ResponseEntity<CharacterSheet> create(@RequestBody CharacterSheet created) {
-		return new ResponseEntity<CharacterSheet>(service.create(created), HttpStatus.CREATED);			
+		return new ResponseEntity<CharacterSheet>(service.create(created), HttpStatus.CREATED);
+	}
+
+	@PutMapping("/update")
+	public ResponseEntity<CharacterSheet> updateCharacterSheet(@PathParam("id") Long id,
+			@RequestBody CharacterSheet updated) {
+		return new ResponseEntity<CharacterSheet>(service.update(id, updated), HttpStatus.OK);
+	}
+
+	@GetMapping("/get/{id}")
+	public ResponseEntity<CharacterSheet> getCharacterSheet(@PathVariable Long id) {
+		return new ResponseEntity<CharacterSheet>(service.getCharacterSheet(id), HttpStatus.OK);
+
+	}
+
+	@GetMapping("/getAll")
+	public ResponseEntity<List<CharacterSheet>> getAll() {
+		return new ResponseEntity<List<CharacterSheet>>(service.getAll(), HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/qa/project2/services/CharacterSheetService.java
+++ src/main/java/com/qa/project2/services/CharacterSheetService.java
@@ -2,8 +2,9 @@ package com.qa.project2.services;
 
 import com.qa.project2.repository.CharacterSheetRepo;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
 import com.qa.project2.domain.CharacterSheet;
 
 @Service
@@ -19,6 +20,30 @@ public class CharacterSheetService {
 	public CharacterSheet create(CharacterSheet created) {
 		return repo.save(created);
 		
+	}
+	
+	public CharacterSheet update(Long id, CharacterSheet updated) {
+		
+		CharacterSheet existing = repo.findById(id).get();
+		
+		existing.setForename(updated.getForename());
+		existing.setSurname(updated.getSurname());
+		existing.setLevel(updated.getLevel());
+		existing.setCharClass(updated.getCharClass());
+		existing.setRace(updated.getRace());
+		existing.setAlignment(updated.getAlignment());
+		existing.setBackround(updated.getBackround());
+		existing.setGender(updated.getGender());
+		
+		return repo.save(existing);
+	}
+
+	public CharacterSheet getCharacterSheet(Long id) {
+		return repo.findById(id).get();
+	}
+
+	public List<CharacterSheet> getAll() {
+		return repo.findAll();
 	}
 
 }
