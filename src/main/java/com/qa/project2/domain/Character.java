46380f13f359f724cf5f5ba08795ae370cde8b17
Matthew Duck <MatthewDDuck@googlemail.com>
2022-05-30 10:35:18 +0100

    first commit

---
+++ src/main/java/com/qa/project2/domain/Character.java
@@ -0,0 +1,68 @@
+package com.qa.project2.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Character {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	@NotNull
+	private String forename;
+
+	@Column
+	private String surname;
+
+	@Column
+	@NotNull
+	private int level;
+
+	@Column
+	@NotNull
+	private String charClass;
+
+	@Column
+	@NotNull
+	private String race;
+
+	@Column
+	@NotNull
+	private String alignment;
+
+	@Column
+	@NotNull
+	private String backround;
+	
+	@Column
+	@NotNull
+	private String gender;
+
+	public Character(String forename, String surname) {
+		this.forename = forename;
+		this.surname = surname;
+
+	}
+
+}
