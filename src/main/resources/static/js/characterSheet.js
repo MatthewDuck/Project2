d756e72e3a915624d4e2431852810f00f9be9d16
Matthew Duck <MatthewDDuck@googlemail.com>
2022-06-01 09:06:26 +0100

    Fixed accordion

--- src/main/resources/static/js/characterSheet.js
+++ src/main/resources/static/js/characterSheet.js
@@ -8,7 +8,7 @@ let form = document.querySelector("form");
 let count = 0;
 
 let getAll = () => {
-    displayDiv.innerHTML="";
+    displayDiv.innerHTML = "";
     axios.get("http://localhost:8080/charactersheet/getAll")
         .then((response) => {
             console.log(response);
@@ -64,63 +64,58 @@ let update = (id) => {
 let displayCharacter = (data) => {
     for (let entry of data) {
         count++;
-        
-        const characterDetails = document.createTextNode(`${entry.level} ${entry.charClass} ${entry.race} ${entry.alignment} ${entry.background} ${entry.gender}`);
 
         let accordionItem = document.createElement("div");
         accordionItem.setAttribute("class", "accordion-item");
 
         let accordionHeader = document.createElement("h2");
         accordionHeader.setAttribute("class", "accordion-header");
-        accordionHeader.setAttribute("id", "flush-heading"+count);
+        accordionHeader.setAttribute("id", "flush-heading" + count);
 
-        let accordionBtn = document.createElement("button");
-        accordionBtn.setAttribute("class", "accordion-button collapsed");
-        accordionBtn.setAttribute("type", "button");
-        accordionBtn.setAttribute("date-bs-toggle", "collapse");
-        accordionBtn.setAttribute("data-bs-target", "#flush-collapse"+count);
-        accordionBtn.setAttribute("aria-expanded", "false");
-        accordionBtn.setAttribute("aria-controls", "flush-collapse"+count);
-        accordionBtn.textContent = `${entry.forename} ${entry.surname}`;
+        let accordionButton = document.createElement("button");
+        accordionButton.setAttribute("class", "accordion-button collapsed");
+        accordionButton.setAttribute("type", "button");
+        accordionButton.setAttribute("data-bs-toggle", "collapse");
+        accordionButton.setAttribute("data-bs-target", "#flush-collapse" + count);
+        accordionButton.setAttribute("aria-expanded", "false");
+        accordionButton.setAttribute("aria-controls", "flush-collapse" + count);
 
         let flushCollapse = document.createElement("div");
-        flushCollapse.setAttribute("id", "flush-collapse"+count);
+        flushCollapse.setAttribute("id", "flush-collapse" + count);
         flushCollapse.setAttribute("class", "accordion-collapse collapse");
-        flushCollapse.setAttribute("aria-labelledby", "flush-heading"+count);
+        flushCollapse.setAttribute("aria-labelledby", "flush-heading" + count);
         flushCollapse.setAttribute("data-bs-parent", "#accordionFlushExample");
 
         let accordionBody = document.createElement("div");
         accordionBody.setAttribute("class", "accordion-body");
 
-        // let updateBtn = document.createElement("button");
-        // updateBtn.setAttribute("id", "btnUpdate");
-        // updateBtn.setAttribute("type", "button");
-        // updateBtn.setAttribute("class", "btn btn-outline-dark");
-        // updateBtn.innerHTML = "Update Character";
+        let updateBtn = document.createElement("button");
+        updateBtn.setAttribute("id", "btnUpdate");
+        updateBtn.setAttribute("type", "button");
+        updateBtn.setAttribute("class", "btn btn-outline-dark");
+        updateBtn.innerHTML = "Update Character";
 
-        // let deleteBtn = document.createElement("button");
-        // deleteBtn.setAttribute("id", "btnDelete");
-        // deleteBtn.setAttribute("type", "button");
-        // deleteBtn.setAttribute("class", "btn btn-outline-dark");
-        // deleteBtn.innerHTML = "Delete Character";
+        let deleteBtn = document.createElement("button");
+        deleteBtn.setAttribute("id", "btnDelete");
+        deleteBtn.setAttribute("type", "button");
+        deleteBtn.setAttribute("class", "btn btn-outline-dark");
+        deleteBtn.innerHTML = "Delete Character";
 
-        //let buttonDiv = document.createElement("div");
+        let buttonDiv = document.createElement("div");
 
-       
-        accordionHeader.appendChild(accordionBtn);
-        accordionItem.appendChild(accordionHeader);
-        //buttonDiv.appendChild(updateBtn);
-        //buttonDiv.appendChild(deleteBtn);
-        accordionBody.appendChild(characterDetails);
-        //accordionBody.appendChild(buttonDiv);
+        accordionButton.textContent = `${entry.forename} ${entry.surname}`
+        accordionBody.textContent = `${entry.forename} ${entry.surname} is a level ${entry.level} ${entry.gender} ${entry.race} ${entry.charClass}.  They are ${entry.alignment} and spent their time as a ${entry.background} before becoming an adventurer.`;
+
+        buttonDiv.appendChild(updateBtn);
+        buttonDiv.appendChild(deleteBtn);
+        accordionBody.appendChild(buttonDiv);
+
+        accordionHeader.appendChild(accordionButton);
         flushCollapse.appendChild(accordionBody);
+        accordionItem.appendChild(accordionHeader);
         accordionItem.appendChild(flushCollapse);
-        displayDiv.appendChild(accordionItem);
 
-        //deleteBtn.addEventListener("click", function () { deleteId(entry.id); });
-       // updateBtn.addEventListener("click", function () { update(entry.id); });
-        
-        
+        displayDiv.appendChild(accordionItem);
     }
 }
 
