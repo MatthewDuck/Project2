5ed298de2dca9968c4d0ed2ec920f6b879d14277
Matthew Duck <MatthewDDuck@googlemail.com>
2022-06-07 09:41:26 +0100

    ignored lombok for testing coverage

--- src/main/java/com/qa/project2/domain/CharacterSheet.java
+++ src/main/java/com/qa/project2/domain/CharacterSheet.java
@@ -9,17 +9,17 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Data
+@Generated
 @Entity
-@Getter
-@Setter
-@ToString
-@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "charactersheet")
@@ -64,19 +64,6 @@ public class CharacterSheet {
 	@Column
 	private String charImage;
 
-//	public CharacterSheet(String forename, String surname, int level, String charClass, String race, String alignment, String background, String gender) {
-//		this.forename = forename;
-//		this.surname = surname;
-//		this.level=level;
-//		this.charClass=charClass;
-//		this.race=race;
-//		this.alignment=alignment;
-//		this.background=background;
-//		this.gender=gender;
-//
-//	}
-	
-	//Test for image
 	public CharacterSheet(String forename, String surname, int level, String charClass, String race, String alignment, String background, String gender, String charImage) {
 		this.forename = forename;
 		this.surname = surname;
@@ -89,5 +76,4 @@ public class CharacterSheet {
 		this.charImage=charImage;
 
 	}
-
 }
